a0530222a3377b37b08f32a5b6ee462e625ebe2e
GAGITUMAD <ahmadrifkyy48@gmail.com>
2026-02-11 09:15:58 +0700

    Add files via upload

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+ return (
+ <section className="section">
+ <h2>Tentang Saya</h2>
+    <p>
+ Saya adalah siswa kelas XI Rekayasa
+Perangkat Lunak yang memiliki minat
+ di bidang pemrograman web dan pengembangan
+aplikasi.
+    </p>
+
+ <p>Nama: Rifky Ahmad Maolana</p>
+<p>Kelas: XI RPL A</p>
+ <p>Sekolah: SMK PusdikHubad Cimahi</p>
+
+ </section>
+ );
+}
